1bdf918a24e2df407642ec7ff9e59169aa42d753
Ivan Avagardo <69362379+SignaturaVeter@users.noreply.github.com>
2021-05-13 10:35:40 +0300

    template
    no js, soon

---
+++ 1-network/scripts.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector('.wrapper'),
+  toast = wrapper.querySelector('.toast'),
+  wifiIcon = wrapper.querySelector('.icon'),
+  title = wrapper.querySelector('span'),
+  subTitle = wrapper.querySelector('p');
